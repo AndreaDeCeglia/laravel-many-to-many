0c5cffb613001a24ce344eda84d7de5fba7a37f8
AndreaDeCeglia <deceglia89@gmail.com>
2023-01-28 21:16:51 +0100

    table on index.blade.php done

--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -2,16 +2,50 @@
 
 @section('content')
 
-    <h3>
-        BenTornato {{ $user->name }}
-    </h3>
-    
-    <h1>
-        qui stamperò i post
-    </h1>
+    <div class="container">
 
-    @foreach ($posts as $elem)
-        {{ $elem->title }}
-    @endforeach
+        <h3>
+            BenTornato {{ $user->name }}
+        </h3>
+        
+        <h1>
+            qui stamperò i post
+        </h1>
+    
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#id</th>
+                    <th scope="col">title</th>
+                    <th scope="col">body</th>
+                    <th scope="col">actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($posts as $elem)
+                <tr>
+                    <td>{{$elem->id}}</td>
+                    <td>{{$elem->title}}</td>
+                    <td>{{$elem->body}}</td>
+                    {{-- <td>
+                        <form action="{{route('posts.destroy', $elem->id)}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-dark" type="submit">
+                                X
+                            </button>
+                        </form>
+                        <div>
+                            <a href="{{route('posts.edit', $elem->id)}}">
+                                 EdiT
+                            </a>
+                        </div>
+                    </td> --}}
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    
 
 @endsection
