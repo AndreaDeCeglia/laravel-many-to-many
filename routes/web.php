6b87a58833e1ed034a942080a312c64d69329bc1
AndreaDeCeglia <deceglia89@gmail.com>
2023-01-28 13:36:26 +0100

    first tables migrated

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//gestisce le rotte sotto il punto di vista di autenticazione
+//definire quale middleware abbiamo creato e richiamiamo
+Route::middleware('auth')
+    ->namespace('Admin') //Cartella in cui si trovano i controller che indico qui cosi non lo riscrivo nelle route
+    ->prefix('admin') // il mio localhost prenderà 8080/admin/   che diventa url base
+    ->name('admin.') // da il nome al prefisso dei name delle rotte
+    ->group(function(){ //nella funzione group(), vado a scrivere tutti quei controller che saranno all'interno della cartella Admin, e che hanno bisogno di autenticazione
+        Route::get('/', 'HomeController@index')->name('index');
+    });
+
+
+//trovare un modo per gestire tutte le rotte che non utilizzano l'autenticazione
+
